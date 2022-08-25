b57a38724e1a30eeb7b4456878def3337d726af7
fahryt <111506384+fahryt@users.noreply.github.com>
2022-08-25 09:57:26 -0700

    exercises 6 - 19 uploaded

---
+++ Exercises19.java
@@ -0,0 +1,11 @@
+package chapters.Chapter02.Exercises;
+
+import java.util.Scanner;
+
+public class Exercises19 {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter three points for a triangle: ");
+		double side1
+	}
+}
